b1e2ff589ee152606cf02c293ec7a708f63b662c
stlueno <stl.uenokouki@gmail.com>
2018-03-27 12:18:26 +0900

    作成中 2018/03/27 AM10:00 二回目のコミット 2018/03/27 PM12:16

---
+++ app/src/main/java/com/example/stl/carquiz/HelpActivity.java
@@ -0,0 +1,19 @@
+package com.example.stl.carquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
